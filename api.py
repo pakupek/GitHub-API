d75a41a94683fa41a957fc38aaa68f034a69fb96
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-06-26 20:56:40 +0200

    fixed contributors count

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import requests
-import json, urllib.request
-ACCESS_KEY = "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"
+import json
+from urllib.request import urlopen
+ACCESS_KEY = "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"
 
 
 def get_list_repo(user):
@@ -32,67 +33,41 @@ def get_urls(user):
         list_url.append(url)
     return list_url
 
-'''
-def get_contributor(user):
-    list_repo = get_list_repo(user)
-    list_url =[]
-    list_contributors =[]
-    for repository, url in zip(list_repo,list_url):
-        base = "https://api.github.com/repos/{}".format(user)
-        extend = '/{}/contributors?per_page=100'.format(repository["name"])
-        url = base+extend
-        list_url.append(url)
-        header ={
-                "Authorization": f'token {ACCESS_KEY}'
-        }
-        response = requests.get(url, header)
-        response.raise_for_status()
-        result = len(response.json())
-        list_contributors.append(result)
-        
-        return list_contributors
-    print(list_url)
-    
-    
-    for url in list_url:
-        endpoint = url   
-        header ={
-        "Authorization": f'token {ACCESS_KEY}'
-        }
-        response = requests.get(endpoint, header)
-        response.raise_for_status()
-        result = len(response.json())
-        list_contributors.append(result)
-        return len(response.json())
-    return list_contributors
-    '''
 
 def get_contributor(user,urls):
     list_repo = get_list_repo(user)
     urls = get_urls(user)
     list_contributors = []
-    for url in urls:   
-        header ={
+    header ={
         "Authorization": f'token {ACCESS_KEY}'
         }
-        r = requests.get(url, header)
-        result = len(r.json())
-        if len(r.json()) is None:
-            list_contributors.append(result)
-        print(result)
-        list_contributors.append(result)
-    
+    for url in urls:   
+        r = requests.get(url,header)
+        try:
+            length = len(r.json())
+            list_contributors.append(length)
+        except json.decoder.JSONDecodeError:
+            empty = 0
+            list_contributors.append(empty)
     return list_contributors
     
 
-
-
-'''
-def contributors(user):
-    list_url = get_urls(user)
-    list_contributors =[]
-    for url in list_url:
-        contributor = get_contributor(user='KentBeck')
-        list_contributors.append(contributor)
-    return list_contributors
-'''
+def get_default_branch(user):
+    list_repo = get_list_repo(user)
+    branch_list = []
+    header ={
+        "Authorization": f'token {ACCESS_KEY}'
+        }
+    for repository in list_repo:
+        endpoint ="https://api.github.com/repos/{}/{}/branches".format(user,repository['name'])
+        response = requests.get(endpoint,header)
+        json_response = response.json()
+        print(json_response[""]["name"])
+       
+        
+       
+        
+       
+        
+        
+    return branch_list
--- run.py
+++ run.py
@@ -9,29 +9,14 @@ app=Flask(__name__)
 @app.route("/")
 def table():
     list_repo = api.get_list_repo(user='KentBeck')
+    #print(list_repo)
     list_urls = api.get_urls(user='KentBeck')
     contributor_list = api.get_contributor(user='KentBeck',urls=list_urls)
-    print(contributor_list)
+    branch_list = api.get_default_branch(user='KentBeck')
     
-    #list_urls = api.get_urls(user='KentBeck')
-    #print(api.get_contributor(user='KentBeck'))
-    data = zip(list_repo,contributor_list)
-    '''
-    with open('contributors.json', 'w', encoding='utf8')as file:
-        for repository, contributor in data:
-            result = [{repository["name"]: contributor}]
-            json.dump(result,file)
-
-
     
-    for repository in list_repo:
-        repositories = {}
-        repositories["name"] = repository["name"]
-        add_repositories(repositories)
-    def add_repositories(repositories):
-        data["data"].append(repositories)
-        print(data)
-    '''
+    data = zip(list_repo,contributor_list,branch_list)
+   
     
     
     return render_template('home.html', data=data)
